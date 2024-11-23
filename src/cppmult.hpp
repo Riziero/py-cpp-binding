f132f58043c3f6c4f98f8281df3ec4b6d0e4965e
Riziero <francescodimizio@gmail.com>
2024-11-23 14:46:04 +0100

    it works

--- src/cppmult.hpp
+++ src/cppmult.hpp
@@ -1,3 +1,5 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <vector>
 #define EXPORT_SYMBOL
 
@@ -7,3 +9,11 @@ extern "C" {
     EXPORT_SYMBOL std::vector<double> sort_array(const std::vector<double>& input);
 }
 
+namespace py = pybind11;
+
+PYBIND11_MODULE(example,m)
+{
+  m.doc() = "pybind11 example plugin";
+
+  m.def("sort_array", &sort_array, "Multiply all entries of a list by 2.0");
+}
--- test.py
+++
@@ -1,9 +0,0 @@
-
-import pybind11_example
-
-A = [1.2,2.,3.,4.]
-
-B = example.modify(A)
-
-print(B)
-

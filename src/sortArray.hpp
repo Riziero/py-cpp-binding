bc4e5a4e3a86b00bce91b5eeedcafc50d8175622
Riziero <francescodimizio@gmail.com>
2024-11-26 15:23:23 +0100

    code cleanup

--- src/sortArray.hpp
+++ src/sortArray.hpp
@@ -4,16 +4,13 @@
 #define EXPORT_SYMBOL
 
 extern "C" {
-    EXPORT_SYMBOL float cppmult(int int_param, float float_param);
-    EXPORT_SYMBOL void sorting(int* arr, int size);
     EXPORT_SYMBOL std::vector<double> sort_array(const std::vector<double>& input);
 }
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(pysort,m)
+PYBIND11_MODULE(pysort, m)
 {
   m.doc() = "pybind11 example plugin";
-
   m.def("sort_array", &sort_array, "Multiply all entries of a list by 2.0");
 }

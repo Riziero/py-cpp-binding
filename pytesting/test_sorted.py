f28c6554e859f20c352df22f3f1dfb0d4859b5e6
Riziero <francescodimizio@gmail.com>
2024-11-26 22:37:50 +0100

    add TyperError exception

--- pytesting/test_sorted.py
+++ pytesting/test_sorted.py
@@ -3,8 +3,9 @@ import pytest
 
 def test_sorted():
     A = [9., 1.2, 2., 3., 4.]
-
     B = pysort.sort_array(A)
-    if sorted(A) != B:
-        # Make it fail
-        assert True==False
+    assert B == sorted(A)
+
+def test_raise_type_error():
+     with pytest.raises(TypeError):
+          pysort.sort_array([1, '3', 1, '2'])
